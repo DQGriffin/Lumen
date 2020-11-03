7dc8b7ff5bdffaaa3e85a62de92dc2f806cfcacd
DQuavius Griffin <dquaviusgriffin@gmail.com>
2020-11-02 21:30:49 -0500

    Conform to new LightAction API

--- Lumen/ViewModels/MagicLightViewModel.swift
+++ Lumen/ViewModels/MagicLightViewModel.swift
@@ -13,19 +13,64 @@ class MagicLightViewModel: ObservableObject {
     let magicLightManager = MagicLightManager()
     var connectionStatus: ConnectionStatus = .disconnected
     @AppStorage("brightness") var currentBrightnessValue = 0
+    @AppStorage("active-action-static-id") var activeActionStaticID = ""
     
     init() { 
         actions = [LightAction]()
         magicLightManager.delegate = self
-        actions.append(LightAction(title: "Magic Light", description: "0%", action: setBrightness))
-        actions.append(LightAction(title: "Magic Light", description: "3%", action: setBrightness))
-        actions.append(LightAction(title: "Magic Light", description: "5%", action: setBrightness))
-        actions.append(LightAction(title: "Magic Light", description: "10%", action: setBrightness))
-        actions.append(LightAction(title: "Magic Light", description: "20%", action: setBrightness))
-        actions.append(LightAction(title: "Magic Light", description: "30%", action: setBrightness))
-        actions.append(LightAction(title: "Magic Light", description: "60%", action: setBrightness))
-        actions.append(LightAction(title: "Magic Light", description: "100%", action: setBrightness))
-        setActionActive(withDescription: String("\(currentBrightnessValue)%"))
+        actions.append(LightAction(title: "Magic Light", description: "0%", white: 0, action: performAction(action:)))
+        actions.append(LightAction(title: "Magic Light", description: "3%", white: 3, action: performAction(action:)))
+        actions.append(LightAction(title: "Magic Light", description: "5%", white: 5, action: performAction(action:)))
+        actions.append(LightAction(title: "Magic Light", description: "10%", white: 10, action: performAction(action:)))
+        actions.append(LightAction(title: "Magic Light", description: "20%", white: 20, action: performAction(action:)))
+        actions.append(LightAction(title: "Magic Light", description: "30%", white: 30, action: performAction(action:)))
+        actions.append(LightAction(title: "Magic Light", description: "60%", white: 60, action: performAction(action:)))
+        actions.append(LightAction(title: "Magic Light", description: "100%", white: 100, action: performAction(action:)))
+        actions.append(LightAction(title: "Debug", description: "This is a very long description", action: performAction(action:)))
+        print("Initializer | Active Action Static ID: \(activeActionStaticID)")
+        //setActionActive(withID: activeActionID)
+        setActionActive(withStaticID: activeActionStaticID)
+    }
+    
+    func performAction(withID id: Int) {
+        
+        var selectedAction: LightAction?
+        
+        for action in actions {
+            if action.id == id {
+                selectedAction = action
+                break
+            }
+        }
+        
+        if let safeAction = selectedAction {
+            performAction(action: safeAction)
+        }
+    }
+    
+    func performAction(action: LightAction) {
+        if action.isRGB {
+            print("This is an RGB Light Action")
+        }
+        else {
+            print("This is not an RGB Light Action")
+            performWhiteAction(action: action)
+        }
+    }
+    
+    fileprivate func performRGBAction(action: LightAction) {
+        
+    }
+    
+    fileprivate func performWhiteAction(action: LightAction) {
+        print("Value: \(action.white)")
+        print("Adjusted Value: \(action.adjustedWhite)")
+        let data = Data(bytes: [action.adjustedWhite], count: 1)
+        magicLightManager.setBrightness(to: data)
+        currentBrightnessValue = action.white
+        clearActiveAction()
+        setActionActive(withID: action.id)
+        activeActionStaticID = action.staticIdentifier
     }
     
     func setBrightness(to targetBrightness: String) {
@@ -34,22 +79,41 @@ class MagicLightViewModel: ObservableObject {
         let data = Data(bytes: [adjustedBrightness], count: 1)
         magicLightManager.setBrightness(to: data)
         currentBrightnessValue = Int.init(brightness)!
-        setActionActive(withDescription: targetBrightness)
+        //setActionActive(withDescription: targetBrightness)
     }
     
-    func setActionActive(withDescription description: String) {
+    func setActionActive(withID id: Int) {
         for index in 0..<actions.count {
-            if actions[index].description == description {
+            if actions[index].id == id {
                 actions[index].isActive = true
             }
             else {
                 actions[index].isActive = false
             }
-            objectWillChange.send()
         }
+        objectWillChange.send()
     }
     
-    func getAdjustedBrightnessValue(from value: Int) -> Int {
+    func setActionActive(withStaticID id: String) {
+        for index in 0..<actions.count {
+            if actions[index].staticIdentifier == id {
+                actions[index].isActive = true
+            }
+            else {
+                actions[index].isActive = false
+            }
+        }
+        objectWillChange.send()
+    }
+    
+    func clearActiveAction() {
+        for index in 0..<actions.count {
+            actions[index].isActive = false
+        }
+        objectWillChange.send()
+    }
+    
+    fileprivate func getAdjustedBrightnessValue(from value: Int) -> Int {
         var adjustedValue = value * 255
         adjustedValue = adjustedValue / 100
         return adjustedValue
