ebc3440fefd537e5b23c85189dc32c3612b11336
DQuavius Griffin <dquaviusgriffin@gmail.com>
2020-11-03 19:59:55 -0500

    Indicate RGB light actions with overlay

--- Lumen/View/MagicLightButtonView.swift
+++ Lumen/View/MagicLightButtonView.swift
@@ -15,22 +15,27 @@ struct MagicLightButtonView: View {
             Text(action.description)
         }
         .groupBoxStyle(DefaultGroupBoxStyle())
-        .overlay(action.isActive ? ButtonViewOverlay() : nil)
+        .overlay(action.isRGB ? ButtonViewOverlay(color: action.rgbColor, opacity: 0.004) : nil)
+        .overlay(action.isActive ? ButtonViewOverlay(color: .blue, opacity: 0.2) : nil)
         .animation(.easeInOut(duration: 0.1))
     }
 }
 
 struct ButtonViewOverlay: View {
+    
+    let color: Color
+    let opacity: Double
+    
     var body: some View {
         Rectangle()
-            .foregroundColor(.blue)
-            .opacity(0.2)
+            .foregroundColor(color)
+            .opacity(opacity)
             .cornerRadius(5.0)
     }
 }
 
 struct MagicLightButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        MagicLightButtonView(action: LightAction(title: "MagicLight", description: "15%"))
+        MagicLightButtonView(action: LightAction(title: "Magic Light", description: "15%"))
     }
 }
