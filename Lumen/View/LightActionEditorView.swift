d93c28c7f5dc7c1c0e09cd539a6c3e833f3b6892
DQuavius Griffin <dquaviusgriffin@gmail.com>
2020-11-06 12:43:09 -0500

    Add LightActionEditorView

---
+++ Lumen/View/LightActionEditorView.swift
@@ -0,0 +1,89 @@
+//
+//  LightActionEditorView.swift
+//  Lumen
+//
+//  Created by Dquavius Griffin on 11/6/20.
+//
+
+import SwiftUI
+
+struct LightActionEditorView: View {
+    
+    var action: LightAction
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Inspect")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+                Button(action: {}) {
+                    Text("Done")
+                        .foregroundColor(.blue)
+                        .padding()
+                }
+                .cornerRadius(4)
+                .border(Color.blue, width: 2)
+            }
+            .padding()
+            VStack {
+                HStack {
+                    Text("Title: ")
+                        .font(.title)
+                    Spacer()
+                    Text(action.title)
+                }
+                .padding(.horizontal)
+                HStack {
+                    Text("Description: ")
+                        .font(.title)
+                    Spacer()
+                    Text(action.description)
+                }
+                .padding(.horizontal)
+                HStack {
+                    Text("Red: ")
+                        .font(.title)
+                    Spacer()
+                    Text("\(action.r)")
+                }
+                .padding(.horizontal)
+                HStack {
+                    Text("Green: ")
+                        .font(.title)
+                    Spacer()
+                    Text("\(action.g)")
+                }
+                .padding(.horizontal)
+                HStack {
+                    Text("Blue: ")
+                        .font(.title)
+                    Spacer()
+                    Text("\(action.b)")
+                }
+                .padding(.horizontal)
+                HStack {
+                    Text("White: ")
+                        .font(.title)
+                    Spacer()
+                    Text("\(action.white)")
+                }
+                .padding(.horizontal)
+            }
+            Spacer()
+        }
+    }
+}
+
+
+struct LightActionEditorView_Previews: PreviewProvider {
+    static var previews: some View {
+        var action = LightAction(title: "Magic Light", description: "RGB Blue", red: 0, green: 0, blue: 255, action: previewTestFunction(action:))
+        LightActionEditorView(action: action)
+        //        LightActionEditorView(action: nil)
+    }
+    
+    static func previewTestFunction(action: LightAction) {
+    }
+}
