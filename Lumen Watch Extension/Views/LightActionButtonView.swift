62206a0361c1c7f69e9e331aab9428f0c48c186f
DQuavius Griffin <dquaviusgriffin@gmail.com>
2020-11-07 21:06:37 -0500

    Initial commit

---
+++ Lumen Watch Extension/Views/LightActionButtonView.swift
@@ -0,0 +1,27 @@
+//
+//  LightActionButtonView.swift
+//  Lumen Watch Extension
+//
+//  Created by Dquavius Griffin on 11/7/20.
+//
+
+import SwiftUI
+
+struct LightActionButtonView: View {
+    let action: LightAction
+    
+    var body: some View {
+        Button(action: {}) {
+            Text(action.description)
+        }
+    }
+}
+
+struct LightActionButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        LightActionButtonView(action: LightAction(title: "Magic Light", description: "15%", red: 0, green: 0, blue: 255, action: previewTestFunction(action:)))
+    }
+    
+    static func previewTestFunction(action: LightAction) {
+    }
+}
