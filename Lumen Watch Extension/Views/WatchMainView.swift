1674d4d38b301e7bb84b7e1cde439888f74ac243
DQuavius J. Griffin <dquaviusgriffin@gmail.com>
2021-02-18 00:46:00 -0500

    Show connection status

--- Lumen Watch Extension/Views/WatchMainView.swift
+++ Lumen Watch Extension/Views/WatchMainView.swift
@@ -13,6 +13,14 @@ struct WatchMainView: View {
     
     var body: some View {
         ScrollView {
+            Circle()
+                .frame(width: 15, height: 15, alignment: .center)
+                .padding(.trailing)
+                .foregroundColor((viewModel.connectionStatus == .connected) ? Color.green : Color.blue)
+                .opacity(0.7)
+            Button(action: {viewModel.attemptToConnect()}) {
+                Text("Connect")
+            }
             LazyVGrid(columns: [.init(), .init()]) {
                 ForEach(viewModel.actions) { action in
                     LightActionButtonView(action: action)
